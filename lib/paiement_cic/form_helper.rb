61243cd09047322b24197a34d6ce39d1dcfe8201
Benoit Garret <benoit.garret@wyvernplatform.com>
2011-06-24 12:06:52 +0200

    Decouple the form helper from the implementation

--- lib/paiement_cic/form_helper.rb
+++ lib/paiement_cic/form_helper.rb
@@ -1,7 +1,7 @@
 ## refactor this
 module PaiementCic::FormHelper
-  def paiement_cic_hidden_fields(order, price, order_transaction, options = {})
-    oa = PaiementCic.config(price, order_transaction.reference)
+  def paiement_cic_hidden_fields(price, order_reference, options = {})
+    oa = PaiementCic.config(price, order_reference)
 
     oMac = PaiementCic.new
     sDate = Time.now.strftime("%d/%m/%Y:%H:%M:%S")
@@ -15,9 +15,9 @@ module PaiementCic::FormHelper
         <input type="hidden" name="montant"           id="montant"        value="' + oa["montant"] + '" />
         <input type="hidden" name="reference"         id="reference"      value="' + oa["reference"].to_s + '" />
         <input type="hidden" name="MAC"               id="MAC"            value="' + chaineMAC + '" />
-        <input type="hidden" name="url_retour"        id="url_retour"     value="' + bank_callback_order_transactions_url + '" />
-        <input type="hidden" name="url_retour_ok"     id="url_retour_ok"  value="' + bank_ok_order_transaction_url(order) + '" />
-        <input type="hidden" name="url_retour_err"    id="url_retour_err" value="' + bank_err_order_transaction_url(order) + '" />
+        <input type="hidden" name="url_retour"        id="url_retour"     value="' + options[:return_url] + '" />
+        <input type="hidden" name="url_retour_ok"     id="url_retour_ok"  value="' + options[:return_url_ok] + '" />
+        <input type="hidden" name="url_retour_err"    id="url_retour_err" value="' + options[:return_url_err] + '" />
         <input type="hidden" name="lgue"              id="lgue"           value="' + oa["lgue"] + '" />
         <input type="hidden" name="societe"           id="societe"        value="' + oa["societe"] + '" />
         <input type="hidden" name="texte-libre"       id="texte-libre"    value="' + oa["texte-libre"] + '" />
